6fff74e00fdf587a03951e067d1304ff02be99c4
Joshua Needham <joshuabneedham@gmail.com>
2017-10-27 17:57:00 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -59,8 +59,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, supply|
     puts "#{season}:".capitalize
     supply.each do |holiday, array|
-      holiday.capitalize 
-      array.capitalize
+      holiday.capitalize
       puts "#{holiday}: #{array.join(", ")}"
     end
   end
